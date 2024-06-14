587f9a5dc52d6ce4290c52b9453efad50dce584e
LINEA.FM <linealivemilano@gmail.com>
2024-06-14 19:31:11 +0200

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -5,7 +5,7 @@ import githubLogo from 'public/images/github-mark-white.svg';
 
 const navItems = [
     { linkText: 'Home', href: '/' },
-    { linkText: 'Schedule', href: '/revalidation' },
+    { linkText: 'Schedule', href: '/schedule' },
     { linkText: 'Video', href: '/image-cdn' },
     { linkText: 'Audio', href: '/edge' },
     { linkText: 'About', href: '/blobs' },
