22d6ecdd645958db68eff8c3de47930a424ce692
linea-milano <linealivemilano@gmail.com>
2024-06-15 11:58:44 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,19 +34,3 @@ module.exports = {
 };
 
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './components/**/*.{js,ts,jsx,tsx,mdx}',
-      './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [require('daisyui')],
-    daisyui: {
-      themes: ["lofi"],
-    }
-  }
-  
