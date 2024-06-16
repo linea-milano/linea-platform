c526724ef9f712ddfd73cb791279e5d0ff3ff126
linea-milano <linealivemilano@gmail.com>
2024-06-16 14:28:59 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,29 @@
-import './globals.css';
-import Header from '../components/header';
+import '../styles/globals.css';
+import { Footer } from '../components/footer';
+import { Header } from '../components/header';
 
 export const metadata = {
-  title: 'Linea Media',
-  description: 'Linea Media Platform',
+    title: {
+        template: '%s | Netlify',
+        default: 'Netlify Starter'
+    }
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" data-theme="lofi">
+            <head>
+                <link rel="icon" href="/favicon.svg" sizes="any" />
+            </head>
+            <body className="antialiased text-white bg-black">
+                <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
+                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
+                        <Header />
+                        <div className="grow">{children}</div>
+                        <Footer />
+                    </div>
+                </div>
+            </body>
+        </html>
+    );
 }
