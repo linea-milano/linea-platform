8a65428ddcaf550a205d2aca8487333d524e2b76
linea-milano <linealivemilano@gmail.com>
2024-06-16 14:13:03 +0200

    test

--- components/header.jsx
+++ components/header.jsx
@@ -1,54 +1,10 @@
-import { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import lineaLogo from 'public/linea-logo.png';
-import githubLogo from 'public/images/github-mark-white.svg';
+import dynamic from 'next/dynamic';
 
-const navItems = [
-    { linkText: 'Home', href: '/' },
-    { linkText: 'Schedule', href: '/schedule' },
-    { linkText: 'Video', href: '/video' },
-    { linkText: 'Audio', href: '/audio' },
-    { linkText: 'About', href: '/about' },
-    { linkText: 'Test', href: '/classics' }
-];
+// Importa il componente HeaderClient dinamicamente senza SSR
+const HeaderClient = dynamic(() => import('./HeaderClient'), {
+  ssr: false,
+});
 
-export function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    return (
-        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
-            <Link href="/">
-                <Image src={lineaLogo} alt="Linea logo" className="logo-menu" />
-            </Link>
-            <button className="lg:hidden" onClick={toggleMenu}>
-                ☰
-            </button>
-            <ul className={`flex flex-wrap gap-x-4 gap-y-1 ${menuOpen ? 'block' : 'hidden'} lg:flex`}>
-                {navItems.map((item, index) => (
-                    <li key={index}>
-                        <Link
-                            href={item.href}
-                            className="inline-block px-1.5 py-1 transition hover:opacity-80 sm:px-3 sm:py-2"
-                        >
-                            {item.linkText}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <div className="flex-grow justify-end hidden lg:flex lg:mr-1">
-                <Link
-                    href="https://github.com/netlify-templates/next-platform-starter"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image src={githubLogo} alt="GitHub logo" className="w-7" />
-                </Link>
-            </div>
-        </nav>
-    );
+export default function Header() {
+  return <HeaderClient />;
 }
